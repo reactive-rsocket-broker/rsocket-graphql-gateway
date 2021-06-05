c0922103a71c493858b412d13097f759c2c88bbe
linux_china <libing.chen@gmail.com>
2021-06-05 02:00:22 -0700

    BookGraphqlServiceImplTest

---
+++ graphql-service-provider/src/test/java/com/alibaba/rsocket/graphql/book/impl/BookGraphqlServiceImplTest.java
@@ -0,0 +1,35 @@
+package com.alibaba.rsocket.graphql.book.impl;
+
+import com.alibaba.rsocket.graphql.book.SpringBootBaseTest;
+import graphql.ExecutionInput;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BookGraphqlServiceImplTest extends SpringBootBaseTest {
+
+    @Autowired
+    private BookGraphqlServiceImpl graphqlService;
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testExecute() {
+        ExecutionInput executionInput = ExecutionInput.newExecutionInput().query("query {\n" +
+                "  bookById(id: \"book-1\") {\n" +
+                "    id\n" +
+                "    name\n" +
+                "    pageCount\n" +
+                "    author {\n" +
+                "      firstName\n" +
+                "      lastName\n" +
+                "    }\n" +
+                "  }\n" +
+                "}").build();
+        Map<String, Object> result = (Map<String, Object>) graphqlService.execute(executionInput).block();
+        assertThat(result).containsKey("bookById");
+        System.out.println(result);
+    }
+}
