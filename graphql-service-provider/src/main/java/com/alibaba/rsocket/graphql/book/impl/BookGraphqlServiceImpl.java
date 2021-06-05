9299becbeffb5699dec0a44ca95b25600fd50bed
linux_china <libing.chen@gmail.com>
2021-06-05 01:56:44 -0700

    BookGraphqlServiceImpl

---
+++ graphql-service-provider/src/main/java/com/alibaba/rsocket/graphql/book/impl/BookGraphqlServiceImpl.java
@@ -0,0 +1,27 @@
+package com.alibaba.rsocket.graphql.book.impl;
+
+import com.alibaba.rsocket.RSocketService;
+import com.alibaba.rsocket.graphql.GraphqlRSocketSupport;
+import com.alibaba.rsocket.graphql.book.BookGraphqlService;
+import graphql.ExecutionInput;
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.CompletableFuture;
+
+
+@RSocketService(serviceInterface = BookGraphqlService.class)
+@Component
+public class BookGraphqlServiceImpl extends GraphqlRSocketSupport implements BookGraphqlService {
+    @Autowired
+    private GraphQL graphQL;
+
+    @Override
+    public Mono<Object> execute(ExecutionInput executionInput) {
+        CompletableFuture<ExecutionResult> future = graphQL.executeAsync(executionInput);
+        return Mono.fromFuture(future).map(ExecutionResult::getData);
+    }
+}
