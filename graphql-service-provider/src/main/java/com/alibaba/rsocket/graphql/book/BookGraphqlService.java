c7ac3f58e4a288dc2db8e29b2e0517010844eee6
linux_china <libing.chen@gmail.com>
2021-06-05 01:56:29 -0700

    BookGraphqlService

---
+++ graphql-service-provider/src/main/java/com/alibaba/rsocket/graphql/book/BookGraphqlService.java
@@ -0,0 +1,11 @@
+package com.alibaba.rsocket.graphql.book;
+
+import com.alibaba.rsocket.graphql.GraphqlRSocketService;
+
+/**
+ * GraphQL Service, execute method needed as entrance
+ *
+ * @author leijuan
+ */
+public interface BookGraphqlService extends GraphqlRSocketService {
+}
