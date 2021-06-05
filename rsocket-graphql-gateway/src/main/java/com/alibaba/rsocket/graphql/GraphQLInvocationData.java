2629a9e567d0d1471b53380450062b4b6b31d4b2
linux_china <libing.chen@gmail.com>
2021-06-05 02:25:49 -0700

    GraphQLInvocationData

---
+++ rsocket-graphql-gateway/src/main/java/com/alibaba/rsocket/graphql/GraphQLInvocationData.java
@@ -0,0 +1,37 @@
+package com.alibaba.rsocket.graphql;
+
+import java.util.Map;
+
+public class GraphQLInvocationData {
+    private String query;
+    private String operationName;
+    private Map<String, Object> variables;
+
+    public GraphQLInvocationData() {
+
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public String getOperationName() {
+        return operationName;
+    }
+
+    public void setOperationName(String operationName) {
+        this.operationName = operationName;
+    }
+
+    public Map<String, Object> getVariables() {
+        return variables;
+    }
+
+    public void setVariables(Map<String, Object> variables) {
+        this.variables = variables;
+    }
+}
