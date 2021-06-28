6711ea0a9bd7ff960054d307b714e4809bf56f51
linux_china <libing.chen@gmail.com>
2021-06-27 20:08:58 -0700

    Author

---
+++ graphql-service-provider/src/main/java/com/alibaba/rsocket/graphql/book/dgs/Author.java
@@ -0,0 +1,40 @@
+package com.alibaba.rsocket.graphql.book.dgs;
+
+public class Author {
+    private String id;
+    private String firstName;
+    private String lastName;
+
+    public Author() {
+    }
+
+    public Author(String id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
