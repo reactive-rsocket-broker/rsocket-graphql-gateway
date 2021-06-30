f0c99c7fbf4cf6484d57a3254946618dd7dc98bc
linux_china <libing.chen@gmail.com>
2021-06-30 00:46:56 -0700

    implement subscribe

--- graphql-rsocket-support/src/main/java/com/alibaba/rsocket/graphql/GraphqlRSocketSupport.java
+++ graphql-rsocket-support/src/main/java/com/alibaba/rsocket/graphql/GraphqlRSocketSupport.java
@@ -5,6 +5,7 @@ import graphql.ExecutionInput;
 import graphql.ExecutionResult;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.nio.charset.StandardCharsets;
@@ -18,35 +19,50 @@ import java.util.Map;
 public abstract class GraphqlRSocketSupport implements GraphqlRSocketExecutor {
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    @SuppressWarnings("unchecked")
-    public Mono<ByteBuf> execute(ByteBuf byteBuf) {
-        String text = byteBuf.toString(StandardCharsets.UTF_8);
+    public Mono<ByteBuf> execute(ByteBuf invocationData) {
         try {
-            Map<String, Object> map = objectMapper.readValue(text, Map.class);
-            ExecutionInput.Builder builder = ExecutionInput.newExecutionInput();
-            String query = (String) map.get("query");
-            String operationName = (String) map.get("operationName");
-            Map<String, Object> variables = (Map<String, Object>) map.get("variables");
-            if (query != null) {
-                builder.query(query);
-            } else {
-                builder.query("");
-            }
-            if (operationName != null) {
-                builder.operationName(operationName);
-            }
-            if (variables != null) {
-                builder.variables(variables);
-            }
-            ExecutionInput executionInput = builder.build();
+            ExecutionInput executionInput = parseInput(invocationData.toString(StandardCharsets.UTF_8));
             return execute(executionInput).map(result -> Unpooled.wrappedBuffer(json(result.toSpecification())));
         } catch (Exception e) {
             return Mono.error(e);
         }
     }
 
+    @Override
+    public Flux<ByteBuf> subscribe(ByteBuf invocationData) {
+        try {
+            ExecutionInput executionInput = parseInput(invocationData.toString(StandardCharsets.UTF_8));
+            return subscribe(executionInput).map(result -> Unpooled.wrappedBuffer(json(result.toSpecification())));
+        } catch (Exception e) {
+            return Flux.error(e);
+        }
+    }
+
+    public abstract Flux<ExecutionResult> subscribe(ExecutionInput executionInput);
+
+
     public abstract Mono<ExecutionResult> execute(ExecutionInput executionInput);
 
+    @SuppressWarnings("unchecked")
+    private ExecutionInput parseInput(String text) throws Exception {
+        Map<String, Object> map = objectMapper.readValue(text, Map.class);
+        ExecutionInput.Builder builder = ExecutionInput.newExecutionInput();
+        String query = (String) map.get("query");
+        String operationName = (String) map.get("operationName");
+        Map<String, Object> variables = (Map<String, Object>) map.get("variables");
+        if (query != null) {
+            builder.query(query);
+        } else {
+            builder.query("");
+        }
+        if (operationName != null) {
+            builder.operationName(operationName);
+        }
+        if (variables != null) {
+            builder.variables(variables);
+        }
+        return builder.build();
+    }
 
     private byte[] json(Object obj) {
         try {
