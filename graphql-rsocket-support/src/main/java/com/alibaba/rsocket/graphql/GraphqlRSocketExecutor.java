2bd6addd75be72194340d861c8944155388b0edb
linux_china <libing.chen@gmail.com>
2021-06-29 19:44:24 -0700

    Add default method

--- graphql-rsocket-support/src/main/java/com/alibaba/rsocket/graphql/GraphqlRSocketExecutor.java
+++ graphql-rsocket-support/src/main/java/com/alibaba/rsocket/graphql/GraphqlRSocketExecutor.java
@@ -1,7 +1,12 @@
 package com.alibaba.rsocket.graphql;
 
 import io.netty.buffer.ByteBuf;
+import org.intellij.lang.annotations.Language;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 /**
  * GraphQL RSocket Executor, execute method needed as entrance
@@ -9,6 +14,30 @@ import reactor.core.publisher.Mono;
  * @author leijuan
  */
 public interface GraphqlRSocketExecutor {
+
+    default Mono<Map<String, Object>> execute(@Language("GraphQL") @NotNull String query) {
+        return execute(query, null, null);
+    }
+
+    default Mono<Map<String, Object>> execute(@Language("GraphQL") String query,
+                                              @NotNull Map<String, Object> variables) {
+        return execute(query, variables, null);
+    }
+
+    /**
+     * execute  GraphQL query, include Query, Mutation and Subscription
+     *
+     * @param query         query document
+     * @param variables     variables
+     * @param operationName operation name
+     * @return map of the result that strictly follows the GraphQL spec
+     */
+    default Mono<Map<String, Object>> execute(@Language("GraphQL") @NotNull String query,
+                                              @Nullable Map<String, Object> variables,
+                                              @Nullable String operationName) {
+        return execute(GraphqlJsonUtils.queryToJsonByteBuf(query, variables, operationName))
+                .map(GraphqlJsonUtils::convertResultToMap);
+    }
 
     /**
      * execute  GraphQL query, include Query, Mutation and Subscription
