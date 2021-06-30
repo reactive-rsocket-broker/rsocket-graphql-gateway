2d3da016282c9a4ddf10517fde30a7c094dfafed
linux_china <libing.chen@gmail.com>
2021-06-29 19:17:51 -0700

    Rename

--- graphql-rsocket-support/src/main/java/com/alibaba/rsocket/graphql/GraphqlRSocketExecutor.java
+++ graphql-rsocket-support/src/main/java/com/alibaba/rsocket/graphql/GraphqlRSocketExecutor.java
@@ -4,17 +4,17 @@ import io.netty.buffer.ByteBuf;
 import reactor.core.publisher.Mono;
 
 /**
- * GraphQL RSocket Service, execute method needed as entrance
+ * GraphQL RSocket Executor, execute method needed as entrance
  *
  * @author leijuan
  */
-public interface GraphqlRSocketService {
+public interface GraphqlRSocketExecutor {
 
     /**
-     * GraphQL execute entrance
+     * execute  GraphQL query, include Query, Mutation and Subscription
      *
      * @param invocationData invocation Map object: query, operationName, variables
-     * @return json with "data" attribute
+     * @return json map of the result that strictly follows the GraphQL spec
      */
     Mono<ByteBuf> execute(ByteBuf invocationData);
 
